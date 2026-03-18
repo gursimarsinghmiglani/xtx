31b457904d108d7b61540a0564eb12729a4d14a3
Gursimar Singh Miglani <gursimarsinghmiglani@gmail.com>
2026-03-18 15:15:12 +0530

    commit

--- include/parser/ast.hpp
+++ include/parser/ast.hpp
@@ -5,19 +5,21 @@
 #include "binary_op_node.hpp"
 #include "node.hpp"
 #include "postfix_op_node.hpp"
+#include "print_node.hpp"
 #include "type_node.hpp"
 #include "unary_op_node.hpp"
 struct AST {
     Node node;
     std::variant<
-        int,
+        long long,
         double,
         bool,
         std::string,
         BinaryOpNode,
         PostfixOpNode,
         TypeNode,
-        UnaryOpNode
+        UnaryOpNode,
+        PrintNode
     > v;
     std::vector<std::unique_ptr<AST>> children;
 };
---
+++ include/parser/print_node.hpp
@@ -0,0 +1,5 @@
+#pragma once
+enum class PrintNode {
+    PRINT,
+    PRINTLN
+};

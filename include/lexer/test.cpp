39859217c986c6a30e6b3f57266e6869e6f1b97f
Gursimar Singh Miglani <gursimarsinghmiglani@gmail.com>
2026-03-18 15:25:09 +0530

    lexer phase complete

--- include/lexer/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "lexer.hpp"
-int main() {
-    std::string source = "while (true) a= .5; \nbreak; continue; A*B; for a in 4..5..-1 { continue; }";
-    std::vector<Lexeme> lexemes = maximal_munch(source);
-    for (Lexeme lex: lexemes) {
-        std::cout << source.substr(lex.start, lex.length) <<  " "  << token_names[static_cast<int>(lex.tok)] << "\n";
-    }
-} 
